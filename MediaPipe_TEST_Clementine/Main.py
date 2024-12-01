7fa53d7d0de421785f99c88433bce18d97041897
Navak94 <44874063+Navak94@users.noreply.github.com>
2024-12-01 10:30:28 -0500

    Create Main.py

---
+++ MediaPipe_TEST_Clementine/Main.py
@@ -0,0 +1,40 @@
+import cv2
+import mediapipe as mp
+from Functions import ProcessHandSteering
+
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+
+cap = cv2.VideoCapture(0)
+
+# Main loop
+while True:
+    # frame is each frame
+    success, frame = cap.read()
+    if success:
+        frame = cv2.flip(frame, 1) 
+        h, w, _ = frame.shape
+
+  
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+		# This is the function that does all the processing
+                turn_pwr, turn_dir = ProcessHandSteering(frame, hand_landmarks, False, h, w)
+                # Print relevant values
+                print(f"Turn Power: {turn_pwr}, Turn Direction: {turn_dir}")
+
+
+        cv2.imshow('Video', frame)
+
+    # Terminate on any key press
+    if cv2.waitKey(1) != -1:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
